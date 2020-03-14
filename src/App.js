db761cdd29b85d456d0f65d6ea3053e061f8820b
unknown <bradbautista@gmail.com>
2020-03-14 19:04:31 -0400

    wireframed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import '@fullcalendar/core/main.css';
 import '@fullcalendar/daygrid/main.css';
 import '@fullcalendar/list/main.css';
 import MediaQuery from 'react-responsive';
+import Modal from 'react-responsive-modal';
 import './App.css';
 // import DATA from './DATA.json';
 import config from './config';
@@ -15,7 +16,10 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      games: []
+      games: [],
+      event: [],
+      favorites: [],
+      open: false
     };
 
   }
@@ -49,6 +53,26 @@ export default class App extends Component {
 
   }
 
+  handleClick = ({ event, el }) => {
+
+    console.log(event._def.title)
+    console.log(el)
+
+    this.setState({ open: true });
+
+    // console.log('Hi!')
+    // console.log(event)
+    // this.toggle();
+    // this.setState({event: event, modal: true})
+
+  }
+ 
+  onCloseModal = () => {
+
+    this.setState({ open: false });
+
+  };
+
   componentDidMount() {
 
     this.fetchGames();
@@ -57,12 +81,9 @@ export default class App extends Component {
 
   render() {
 
-    // const unformattedReleases = DATA.results.filter(release => release.expected_release_day != null)
+    // Styles object for our modal 
+    const styles = { overlay: { background: 'rgba(0, 0, 0, 0.35)' }}
 
-    // console.log(unformattedReleases)
-
-    // { title: release.name, date: release.expected_release_year + expected-release-month + expected-release-day }
-    // date: yyyy-mm-dd (need 0)
 
     const releases = this.state.games.map(release => {
 
@@ -89,24 +110,87 @@ export default class App extends Component {
 
     return (
       <div className="App">
+
         <header className="App-header">
-          <h1>Video Games Release Date Calendar</h1>      
+          <h1>vgCal</h1>      
         </header>
-        <main>
-        <MediaQuery maxWidth={1223}>
-          <FullCalendar 
-            defaultView="listWeek" 
-            plugins={[ listPlugin ]} 
-            events={ releases }
-          />
-        </MediaQuery>
-        <MediaQuery minWidth={1224}>
-          <FullCalendar 
-            defaultView="dayGridMonth" 
-            plugins={[ dayGridPlugin ]} 
-            events={ releases }
-          />
-        </MediaQuery>
+
+        <main>      
+
+        <div className={"calendar-wrapper"}>
+
+          <div className={"sidebar-left"}>
+            <div className={"filtering-container"}>
+              <h2>Filtering options</h2>
+              <input type="checkbox" id="XBox" name="Xbox" value="XBox" />
+              <label for="XBox"> XBOX</label><br/>
+              <input type="checkbox" id="Playstation" name="Playstation" value="Playstation" />
+              <label for="Playstation"> Playstation</label><br/>
+              <input type="checkbox" id="Switch" name="Switch" value="Switch" />
+              <label for="Switch"> Switch</label><br/> 
+              <input type="checkbox" id="PC" name="PC" value="PC" />
+              <label for="PC"> PC</label><br/> 
+            </div>
+            <div className={"favorites-container"}>
+              <h2>Your favorites</h2>
+              <ul>
+                {(this.state.favorites.length === 0)
+                  ? <li>Add a game to your favorites list and you can export an ics file to add the release date to a calendar of your choice</li>
+                  : this.state.favorites
+                }
+              </ul>
+              <button>Export to ICS</button>
+            </div>
+          </div>
+
+          {/* Weekly list view for phones / tablets */}
+          <MediaQuery maxWidth={1199}>
+
+            <FullCalendar 
+              defaultView="listWeek" 
+              plugins={[ listPlugin ]} 
+              events={ releases }
+              eventClick={ this.handleClick }
+              height= { 700 }
+            />
+
+          </MediaQuery>
+
+          {/* Month grid view for laptops/desktops */}
+          <MediaQuery minWidth={1200}>
+
+            <FullCalendar 
+              defaultView="dayGridMonth" 
+              plugins={[ dayGridPlugin ]} 
+              events={ releases }
+              eventClick={ this.handleClick }
+              fixedWeekCount={ false }
+              height={ 950 }
+            />
+            
+          </MediaQuery>
+
+          <div className={"sidebar-right"}>
+            <h2>Landing page info goes here</h2>
+            <p>Welcome to vgCal! Here's some info about how to use the site:</p>
+            <p>Click the left and right arrows above the calendar to change weeks or months. Click "today" to return to the present week or month.</p>
+            <p>Click on a release date to get more information about that release</p>
+            <p>Want to set a reminder for a release? Click on a release, click "Add to favorites" and then click "Export to ICS" in the left-hand sidebar. Your browser will download a calendar file you can import into the calendar of your choosing, or email to yourself. And maybe I'll add a form to let you email it to yourself or something idk yet.</p>
+          </div>
+
+          <Modal 
+            open={this.state.open} 
+            onClose={this.onCloseModal}
+            styles={styles}
+          >
+            <h2 className="modal-heading">Modal containing release information</h2>
+            <p>
+              Details like price, platforms, box art, etc. tbd
+            </p>
+          </Modal>
+
+          </div>
+        
 
         </main>
       </div>
