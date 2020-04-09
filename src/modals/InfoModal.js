9b42117919e920325965cc81aaa95e6a5e9f8d31
unknown <bradbautista@gmail.com>
2020-04-09 17:39:08 -0400

    added collapsible elements to info modal to make info more digestible, also tweaked info modal styling to same end

--- src/modals/InfoModal.js
+++ src/modals/InfoModal.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Context from '../Context';
 import './InfoModal.css';
 import Modal from 'react-responsive-modal';
+import Collapsible from 'react-collapsible';
 import wave from '../images/wave.gif'
 
 export default class InfoModal extends Component {
@@ -38,20 +39,81 @@ export default class InfoModal extends Component {
                     src={wave}
                 />
                 <div className={"info-modal"}>
+
                     <h2 className={"info-modal-header"}>Welcome to vgCal!</h2>
-                    <p className={"info-modal-graf"}>Click the left and right arrows above the calendar to change weeks or months. Click "today" to return to the present week or month.</p>
-                    <p className={"info-modal-graf"}>Click on a release date to get more information about that release.</p>
-                    <p className={"info-modal-prompt"}><strong>Want to set a reminder for a release?</strong></p>
-                    <p className={"info-modal-graf"}>Click on a release, click "Add to favorites" and then click "Export to ICS" in the favorites bar. Your browser will download a calendar file you can import into the calendar of your choosing.</p>
-                    <p className={"info-modal-prompt"}><strong>Want to navigate to a specific date via the URL bar?</strong></p> 
-                    <p className={"info-modal-graf"}>Do it like this: https://vgcal.now.sh/date/12-31-2020</p>
-                    <p className={"info-modal-prompt"}><strong>Want to navigate to a specific game via the URL bar?</strong></p> 
-                    <p className={"info-modal-graf"}>Do it like this: https://vgcal.now.sh/game/Mount-&-Blade-II:-Bannerlord</p>
-                    <p className={"info-modal-prompt"}><strong>I don't see the game I'm looking for.</strong></p> 
-                    <p className={"info-modal-graf"}>If a game has a set release date, it'll show up on the calendar. If it has an ambiguous release date (like September 2020), you can look for it in the searchable dropdown. Still don't see it? vgCal uses the Giant Bomb API to populate its calendar. Add it to the <a className="gb-link" href="https://www.giantbomb.com/games/" target="_blank" rel="noopener noreferrer">Giant Bomb wiki</a> and it'll show up in the calendar — and lots of other places on the Internet!</p>
-                    <p className={"info-modal-prompt"}><strong>Why does the calendar only span 2019 to 2021?</strong></p> 
-                    <p className={"info-modal-graf"}>Right now vgCal's database is size-limited by the database provider. I'm looking into migrating the database to a different provider, which would allow for a much larger dataset.</p>
-                    <p className={"info-modal-prompt"}><strong>Want to see this information again? Click the question-mark button in the header.</strong></p>
+                    <h3 className={"info-modal-lede"}>vgCal helps you find video-game release information quickly.</h3>
+
+                    <p className={"info-modal-graf"}>For more info on how to use the site, click a heading below. To see this info again, click on the question-mark button in the top bar.</p>
+
+                    <Collapsible
+                        trigger={`How do I use this?`}
+                        className="collapsible"
+                        openedClassName="collapsible"
+                        triggerClassName="info-modal-prompt trigger"
+                        triggerOpenedClassName="info-modal-prompt trigger"
+                        transitionTime={275}
+                    >
+                        <p className={"info-modal-graf"}>Click the left and right arrows above the calendar to change weeks or months. Click "today" to return to the present week or month.</p>
+                        <p className={"info-modal-graf"}>Click on a release to get more information about that release.</p>
+                        <p className={"info-modal-graf"}>Know the name of the game but not the date? You can search for it in the searchable dropdown in the top bar.</p>
+                    </Collapsible>
+
+                    <Collapsible
+                        trigger={`What does "Export to ICS" do?`}
+                        className="collapsible"
+                        openedClassName="collapsible"
+                        triggerClassName="info-modal-prompt trigger"
+                        triggerOpenedClassName="info-modal-prompt trigger"
+                        transitionTime={275}
+                    >
+                        <p className={"info-modal-graf"}>Click on a release, click "Add to favorites" and it will show up in your favorites list. With one or more favorites in your list, click "Export to ICS" in the favorites bar. Your browser will download a calendar file you can import into the calendar of your choosing with the release dates as events.</p>
+
+                        <p className={"info-modal-graf"}>vgCal will remember your favorites, but will forget them if you switch devices or browsers.</p>
+                    </Collapsible>
+
+                    <Collapsible
+                        trigger="I want to go to a specific date."
+                        className="collapsible"
+                        openedClassName="collapsible"
+                        triggerClassName="info-modal-prompt trigger"
+                        triggerOpenedClassName="info-modal-prompt trigger"
+                        transitionTime={275}
+                    >
+                        <p className={"info-modal-graf"}>Do it like this: https://vgcal.now.sh/date/12-31-2020</p>
+                    </Collapsible>
+
+                    <Collapsible
+                        trigger="I want to go to a specific game."
+                        className="collapsible"
+                        openedClassName="collapsible"
+                        triggerClassName="info-modal-prompt trigger"
+                        triggerOpenedClassName="info-modal-prompt trigger"
+                        transitionTime={275}
+                    >
+                        <p className={"info-modal-graf"}>Do it like this: https://vgcal.now.sh/game/Mount-&-Blade-II:-Bannerlord</p>
+                    </Collapsible>
+
+                    <Collapsible
+                        trigger="I don't see the game I'm looking for."
+                        className="collapsible"
+                        openedClassName="collapsible"
+                        triggerClassName="info-modal-prompt trigger"
+                        triggerOpenedClassName="info-modal-prompt trigger"
+                        transitionTime={275}
+                    >
+                        <p className={"info-modal-graf"}>If a game has a set release date, it'll show up on the calendar. If it has an ambiguous release date (like September 2020), you can look for it in the searchable dropdown. Still don't see it? vgCal uses the Giant Bomb API to populate its calendar. Add it to the <a className="gb-link" href="https://www.giantbomb.com/games/" target="_blank" rel="noopener noreferrer">Giant Bomb wiki</a> and it'll show up in the calendar — and lots of other places on the Internet!</p>
+                    </Collapsible>
+
+                    <Collapsible
+                        trigger="Why does the calendar only span 2019 to 2021?"
+                        className="collapsible"
+                        openedClassName="collapsible"
+                        triggerClassName="info-modal-prompt trigger"
+                        triggerOpenedClassName="info-modal-prompt trigger"
+                        transitionTime={275}
+                    >
+                        <p className={"info-modal-graf"}>Right now vgCal's database is size-limited by the database provider. I'm looking into migrating the database to a different provider, which would allow for a much larger dataset.</p>
+                    </Collapsible>
                                
                 </div>
           </Modal>
